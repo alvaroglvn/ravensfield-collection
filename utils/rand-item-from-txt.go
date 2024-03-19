4d0c28ea1c0656b41845410478dbedc7ca605ee4
Alvaro Galvan <alvaro@alvarogalvan.com>
2024-03-19 09:59:37 +0000

    cleared small gosec concerns

--- utils/rand-item-from-txt.go
+++ utils/rand-item-from-txt.go
@@ -1,8 +1,11 @@
 package utils
 
 import (
+	"crypto/rand"
 	"fmt"
-	"math/rand"
+	"math/big"
+	"path/filepath"
+
 	"os"
 	"strings"
 )
@@ -10,14 +13,19 @@ import (
 func GetRandFromTxt(file string) (string, error) {
 
 	//generate string from txt
-	content, err := os.ReadFile(file)
+	content, err := os.ReadFile(filepath.Clean(file))
 	if err != nil {
 		return "nil", fmt.Errorf("error reading file: %s", err)
 	}
 	list := strings.Split(string(content), "\n")
 
 	//get random item from list
-	randIndex := rand.Intn(len(list))
+	nBig, err := rand.Int(rand.Reader, big.NewInt(int64(len(list))))
+	if err != nil {
+		return "", fmt.Errorf("error generating rand index: %s", err)
+	}
+
+	randIndex := int(nBig.Int64())
 	randItem := list[randIndex]
 
 	return randItem, nil
