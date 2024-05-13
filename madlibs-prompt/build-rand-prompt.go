170190041d1d5198ad2ac4d48e05e4a235b4712f
Alvaro Galvan <alvaro@alvarogalvan.com>
2024-05-13 17:15:08 +0100

    refactored prompt builder turning txt files into go slices

--- madlibs-prompt/build-rand-prompt.go
+++ madlibs-prompt/build-rand-prompt.go
@@ -1,24 +1,38 @@
 package madlibsprompt
 
 import (
+	"crypto/rand"
 	"fmt"
-
-	"github.com/alvaroglvn/ravensfield-collection/utils"
+	"math/big"
 )
+
+func getRandFromTxt(itemList []string) (string, error) {
+
+	//get random item from list
+	nBig, err := rand.Int(rand.Reader, big.NewInt(int64(len(itemList))))
+	if err != nil {
+		return "", fmt.Errorf("error generating rand index: %s", err)
+	}
+
+	randIndex := int(nBig.Int64())
+	randItem := itemList[randIndex]
+
+	return randItem, nil
+}
 
 func BuildRandPrompt() (string, error) {
 
-	mood, err := utils.GetRandFromTxt("madlibs-prompt/word-lists/genre-mood.txt")
+	mood, err := getRandFromTxt(genreMood)
 	if err != nil {
 		return "", fmt.Errorf("error getting random mood: %s", err)
 	}
 
-	artStyle, err := utils.GetRandFromTxt("madlibs-prompt/word-lists/art-styles.txt")
+	artStyle, err := getRandFromTxt(artStyles)
 	if err != nil {
 		return "", fmt.Errorf("error getting random art style: %s", err)
 	}
 
-	object, err := utils.GetRandFromTxt("madlibs-prompt/word-lists/art-objects.txt")
+	object, err := getRandFromTxt(artObjects)
 	if err != nil {
 		return "", fmt.Errorf("error getting random object: %s", err)
 	}
