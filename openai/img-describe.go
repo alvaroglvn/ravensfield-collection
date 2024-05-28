d04d52c9054f9b4718030444810ce689c01aed0f
Alvaro Galvan <alvaro@alvarogalvan.com>
2024-05-28 08:22:46 +0100

    lowered textgen penalties to 0.25

--- openai/img-describe.go
+++ openai/img-describe.go
@@ -113,8 +113,8 @@ func ImgDescribe(imgURL, openAiKey string) (string, error) {
 			},
 		},
 		MaxTokens:       1000,
-		FreqPenalty:     0.4,
-		PresencePenalty: 0.4,
+		FreqPenalty:     0.25,
+		PresencePenalty: 0.25,
 		Temperature:     1,
 	}
 
